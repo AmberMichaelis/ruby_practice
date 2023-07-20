43c1366d34ac899a8bd5ee80fb0e1b381e3a60c7
Amber M <amberswieczkowski@pm.me>
2023-07-20 18:30:48 -0500

    Start puzzle class. Add initialize method.

--- sudokuSolver.rb
+++ sudokuSolver.rb
@@ -10,3 +10,29 @@
 # puts Sudoku.solve(Sudoku::Puzzle.new(ARGF.realines))
 #
 module Sudoku
+    class Puzzle
+        ASCII = ".123456789"
+        BIN = "\000\001\002\004\005\006\007\010\011"
+
+        def initialize(lines)
+            if (lines.respond_to? :join)
+                s = lines.join
+            else
+                s = lines.dup
+            end
+
+            s.sub!(/\s/. "")
+
+            raise Invalid, "Grid is the wrong size" unless s.size == 81
+
+            if i = s.index(/[^123456789\.]/)
+                raise Invalid, "Illegal character #{s[i,1]} in puzzle"
+            end
+
+            s.tr!(ASCII, BIN)
+            @grid = s.unpack('c*')
+
+            raise Invalid, "Initial puzzle has duplicates" if has_duplicates?
+        end
+    end
+end
