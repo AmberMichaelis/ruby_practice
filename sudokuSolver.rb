5951166b3b7f4361ff5c64d9e1778e49e756f1c0
Amber M <amberswieczkowski@pm.me>
2023-07-21 19:01:25 -0500

    Added more methods to Puzzle class.

--- sudokuSolver.rb
+++ sudokuSolver.rb
@@ -34,5 +34,45 @@ module Sudoku
 
             raise Invalid, "Initial puzzle has duplicates" if has_duplicates?
         end
+
+        def to_s
+            (0..8).collect{|r| @grid[r*9, 9].pack('c9')}.join("\n").tr(BIN, ASCII)
+        end
+
+        def dup
+            copy = super
+            @grid = @grid.dup
+            copy
+        end
+
+        def [](row, col)
+            @grid[row*9 + col]
+        end
+
+        def [] = (row, col, newvalue)
+            unless (0..9).include? newvalue
+                raise Invalid, "illegal cell value"
+            end
+
+            @grid[row*9 + col] = newvalue
+        end
+
+        BoxOfIndex = [
+            0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2,
+            3,3,3,4,4,4,5,5,5,3,3,3,4,4,4,5,5,5,
+            6,6,6,7,7,7,8,8,8,6,6,6,7,7,7,8,8,8
+        ].freeze
+
+        def each_unknown
+            0.upto 8 do |row|
+                0.upto 8 do |col|
+                    index = row*9 + col
+                    next if @grid[index] != 0
+                    box = BoxOfIndex[index]
+                    yield row, col, box
+                end
+            end
+        end
+        
     end
 end
