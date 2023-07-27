315732476bfca5f190f7c1d0070ee1d3b854cd51
Amber M <amberswieczkowski@pm.me>
2023-07-26 23:04:39 -0500

    Add case when def.

--- point.rb
+++ point.rb
@@ -3,7 +3,7 @@ class Point
         @x, @y = x, y
     end
 
-    def x
+   def x
         @x
     end
 
@@ -18,7 +18,28 @@ class Point
 
 end
 
+def coerce(other)
+    [self, other]
+end
+
+def [](index)
+    case index
+    when 0 -2
+    when 1, -1
+    when :x, "x"
+    when :y, "y"
+    else nil
+    end
+end
+
+def each
+    yield @x
+    yield @y
+end
+
 p = Point.new(1,2)
 q = Point.new(p.x*2, p.y*2)
 puts q.to_s
 
+p = Point.new(1,2)
+puts p
