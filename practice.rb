ac0dc3ffed0dcfc0b50c62c85b579e6fc8fbf1df
Amber M <amberswieczkowski@pm.me>
2023-09-16 19:53:57 -0500

    Update practice file.

--- practice.rb
+++ practice.rb
@@ -5,3 +5,9 @@ puts "Hello #{name} my old friend"
 time = 1
 puts "I've come to meet you #{time}ce again"
 
+puts <<PARAGRAPH
+There's something going on here.
+With the PARAGRAPH thing.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+PARAGRAPH
